ae10465a9f8b85cec87b712e4f1ccbed103e2697
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-07-30 15:29:18 -0300

    feat: create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Button from "@/components/button/button";
 import Navbar from "@/components/navbar/navbar";
-
 
 export default function Home() {
   return (
     <div className="bg-gray-800 h-dvh">
     <Navbar/>
+
+            <p className="font-primary text-white text-2xl font-bold">aaaa</p>
+            <Button>testee</Button>
     </div>
   );
 }
